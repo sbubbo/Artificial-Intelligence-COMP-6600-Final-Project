7cd18ba2c4797680208cac72efe624c1c091472d
LibbyWessels <166661899+LibbyWessels@users.noreply.github.com>
2026-03-29 13:22:55 -0500

    created heuristics.py

---
+++ ai/heuristics.py
@@ -0,0 +1,19 @@
+"""
+This file sets up the Connect Four heuristics
+
+Functions of this file include: 
+ - material_count
+ - center_control
+ - threat_detection
+"""
+
+from game.board import *
+
+def material_count(board, token):
+    pass
+
+def center_control(board, token):
+    pass
+
+def threat_detection(board, token):
+    pass
